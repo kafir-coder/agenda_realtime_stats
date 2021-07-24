edd5ce2db6933fccd82d44531f280aa326b8f0b1
Caio <caiotony16@gmail.com>
2021-07-24 17:06:36 +0100

    fix: ensure bee-queue loads all lua scripts after a reboot

--- api/server.ts
+++ api/server.ts
@@ -3,12 +3,12 @@ import BeeQueue from 'bee-queue';
 import processor from './core/processor'
 import dotenv from 'dotenv';
 import Redis from 'ioredis';
-dotenv.config();
 const redis = new Redis({
   host: process.env.REDIS_HOST as string,
   port: process.env.REDIS_PORT as unknown as number,
   password: process.env.REDIS_PASSWORD as string
 });
+console.log(redis)
 export default async function (req: VercelRequest, res: VercelResponse) {
   
   const configurations = {
