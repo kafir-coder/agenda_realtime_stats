0daa8e6a0707b7b3803ebf36a5ea0391bdd91138
RJ Tony <ray.developper@gmail.com>
2021-07-24 15:04:31 +0100

    chore: add the processor class which will be responsible of process all enqueued jobs

--- api/core/processor.ts
+++ api/core/processor.ts
@@ -1,19 +1,25 @@
 import BeeQueue, { Job } from "bee-queue";
-import processor_skelecton from "../protocols/processor";
-import { quee_element } from "../protocols/queue_element";
+import { Redis } from "ioredis";
+import { processor_skelecton, quee_element } from "../protocols";
 
-class processor implements processor_skelecton<quee_element> {
+export default class processor implements processor_skelecton<quee_element> {
   constructor(private readonly Queue: BeeQueue) {}
   async enqueue(element: quee_element): Promise<Job<quee_element>> {
     const job = this.Queue.createJob(element);
     const queued = await job.timeout(3000).retries(2).save();
     return queued;
   }
-  async process(): Promise<Job<quee_element>> {
-    return new Promise(resolve => {
-      this.Queue.process(async function (job: Job<quee_element>) {
-        console.log(`Processing job ${job.id}`);
-        resolve(job)
+  async process(redis: Redis, commandsPipe: Array<quee_element>): Promise<void> {
+    return new Promise(() => {
+      this.Queue.process(async () => {
+        await redis.pipeline([
+          [commandsPipe[0].command, commandsPipe[0].arguments as string],
+          [commandsPipe[1].command, commandsPipe[1].arguments as string],
+          [commandsPipe[2].command, commandsPipe[2].arguments as string],
+          [commandsPipe[3].command, commandsPipe[3].arguments as string],
+          [commandsPipe[4].command, commandsPipe[4].arguments as string],
+          [commandsPipe[5].command, commandsPipe[5].arguments as string],
+        ]).exec()
       });
     })
     
--- api/protocols/processor.ts
+++ api/protocols/processor.ts
@@ -1,6 +1,7 @@
 import { Job } from "bee-queue";
+import { Redis } from "ioredis";
 
-export default interface processor_skelecton<T> {
+export interface processor_skelecton<T> {
   enqueue: (qE: T) => Promise<Job<T>>;
-  process: () => Promise<Job<T>>
+  process: (redis: Redis, commandsPipe: Array<T>) => Promise<void>
 }
